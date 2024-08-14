48e6284d4eae9b581af0b6598c0ae7b3017c51bc
ybw0014 <i@guizhanss.com>
2024-08-14 10:23:55 -0400

    fix(GetGoldSpade): no longer placeable

--- src/main/kotlin/io/github/slimefunguguproject/bump/implementation/items/tools/GetGoldSpade.kt
+++ src/main/kotlin/io/github/slimefunguguproject/bump/implementation/items/tools/GetGoldSpade.kt
@@ -6,7 +6,7 @@ import io.github.thebusybiscuit.slimefun4.api.items.ItemGroup
 import io.github.thebusybiscuit.slimefun4.api.items.SlimefunItemStack
 import io.github.thebusybiscuit.slimefun4.api.recipes.RecipeType
 import io.github.thebusybiscuit.slimefun4.core.handlers.ToolUseHandler
-import io.github.thebusybiscuit.slimefun4.implementation.items.SimpleSlimefunItem
+import io.github.thebusybiscuit.slimefun4.implementation.items.blocks.UnplaceableBlock
 import net.guizhanss.guizhanlib.utils.RandomUtil
 import org.bukkit.Material
 import org.bukkit.enchantments.Enchantment
@@ -19,8 +19,12 @@ class GetGoldSpade(
     itemStack: SlimefunItemStack,
     recipeType: RecipeType,
     recipe: Array<out ItemStack?>
-) : SimpleSlimefunItem<ToolUseHandler>(itemGroup, itemStack, recipeType, recipe) {
-    override fun getItemHandler() =
+) : UnplaceableBlock(itemGroup, itemStack, recipeType, recipe) {
+    init {
+        addItemHandler(getToolUseHandler())
+    }
+
+    private fun getToolUseHandler() =
         ToolUseHandler { e: BlockBreakEvent, item: ItemStack, _: Int, drops: MutableList<ItemStack> ->
             if (e.block.type != Material.SAND) return@ToolUseHandler
 
